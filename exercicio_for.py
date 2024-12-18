7b6cf6abdc475d33327ba1ebb61cb618d8a69a1c
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-18 05:58:40 -0300

    Adicionado exercicio 7

--- exercicio_for.py
+++ exercicio_for.py
@@ -6,29 +6,29 @@
 #  palavra única aparece nele.
 
 
-import string
+# import string
 
-texto_usuario=input("Digite uma frase por favor: ")
+# texto_usuario=input("Digite uma frase por favor: ")
 
-texto_usuario = texto_usuario.lower() 
-for pontuacao in string.punctuation:
-    texto_usuario= texto_usuario.replace(pontuacao, "")
-
-
-divisao_de_palavras= texto_usuario.split(" ")# dividindo o texto em palavras
-contagem_de_palavras = {} # iniciando o dicionario vazio para armazenar a contagem
-
-for palavra_atual in divisao_de_palavras:
-    
-    if palavra_atual in contagem_de_palavras:
-        contagem_de_palavras [palavra_atual] += 1
-    else:
-        contagem_de_palavras[palavra_atual] = 1
+# texto_usuario = texto_usuario.lower() 
+# for pontuacao in string.punctuation:
+#     texto_usuario= texto_usuario.replace(pontuacao, "")
 
 
-print("Contagem de palavras no texto: \n ")
-for palavra_atual, quantidade in contagem_de_palavras.items():
-    print(f"{palavra_atual} : {quantidade}")
+# divisao_de_palavras= texto_usuario.split(" ")# dividindo o texto em palavras
+# contagem_de_palavras = {} # iniciando o dicionario vazio para armazenar a contagem
+
+# for palavra_atual in divisao_de_palavras:
+
+#     if palavra_atual in contagem_de_palavras:
+#         contagem_de_palavras [palavra_atual] += 1
+#     else:
+#         contagem_de_palavras[palavra_atual] = 1
+
+
+# print("Contagem de palavras no texto: \n ")
+# for palavra_atual, quantidade in contagem_de_palavras.items():
+#     print(f"{palavra_atual} : {quantidade}")
 
 
 
@@ -40,6 +40,17 @@ for palavra_atual, quantidade in contagem_de_palavras.items():
 
 ### Exercício 7. Normalização de Dados
 # Objetivo:** Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+
+numeros= [10,20,30,40,50]
+minimo = min(numeros)
+maximo = max(numeros)
+normalizados = []
+
+for valor_atual in numeros:
+    valor_normalizado =(valor_atual - minimo) / (maximo - minimo)
+    normalizados.append(valor_normalizado)
+
+print(normalizados)
 
 ### Exercício 8. Filtragem de Dados Faltantes
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, filtrar aqueles que têm um campo específico faltando
