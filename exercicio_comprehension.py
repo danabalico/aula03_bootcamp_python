ef7d0d7c06e297a6c335f1bddd12299d0cc67440
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-18 11:04:37 -0300

    Adicionado exercicios 1,2 e 3 de uma lista de list comprehension

---
+++ exercicio_comprehension.py
@@ -0,0 +1,60 @@
+
+# 1. Filtrando números pares em uma lista
+# Exercício: Dada uma lista de números, crie uma nova lista 
+# contendo apenas os números pares.
+
+# numeros_gerais = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# nova_lista = [n for n in numeros_gerais if n % 2 == 0]
+# print(nova_lista)
+
+
+
+
+# 2. Multiplicando cada número por 10
+# Exercício: Dada uma lista de números, crie uma nova lista com cada 
+# número multiplicado por 10.
+
+# lista_numeros = [1, 3, 2, 5, 4, 66, 3, 90, 100, 33, 5, 7, 18]
+# lista_nova = [n * 10 for n in lista_numeros]
+# print(lista_nova)
+
+# para cada n (numero da lista_numeros) multiplique por 10
+# e vai fazenedo isso para cada n(numero) que tem na lista_numeros
+
+
+# 3. Convertendo todas as palavras para maiúsculas
+# Exercício: Dada uma lista de palavras, crie uma nova 
+# lista com todas as palavras em letras maiúsculas.
+
+
+# palavras = ['casa', 'carro', 'familia', 'filhos', 'amor', 'comida', 'alegria', 'paz']
+# palavras_maiusculas = [p.upper() for p in palavras ]
+# print(palavras_maiusculas)
+
+# 4. Filtrando nomes com mais de 5 letras
+# Exercício: Dada uma lista de nomes, crie uma nova lista 
+# contendo apenas os nomes com mais de 5 letras.
+
+# 5. Extraindo os domínios de e-mails
+# Exercício: Dada uma lista de e-mails, crie uma nova lista 
+# contendo apenas o domínio dos e-mails (parte após o @).
+
+# 6. Filtrando números negativos
+# Exercício: Dada uma lista de números, crie uma nova 
+# lista contendo apenas os números negativos.
+
+# 7. Contagem de ocorrências de uma palavra em uma lista de strings
+# Exercício: Dada uma lista de frases, crie uma nova lista 
+# com a contagem de ocorrências da palavra "dados".
+
+# 8. Filtrando números que são múltiplos de 3 ou 5
+# Exercício: Dada uma lista de números, crie uma nova lista 
+# contendo apenas os números que são múltiplos de 3 ou 5.
+
+# 9. Extraindo o nome dos usuários de um dicionário de dados
+# Exercício: Dado um dicionário contendo informações de usuários 
+# (nome e email), crie uma lista com os nomes dos usuários.
+
+# 10. Transformando uma lista de números em uma lista de quadrados
+# Exercício: Dada uma lista de números, crie uma nova lista com 
+# os quadrados desses números
