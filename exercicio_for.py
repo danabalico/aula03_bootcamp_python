ef391be4877aa364faabdefdf98eb173580b20b2
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-18 07:22:48 -0300

    Adicionado exercicio 9

--- exercicio_for.py
+++ exercicio_for.py
@@ -58,25 +58,38 @@
 # Objetivo:** Dada uma lista de dicionários representando dados de usuários, 
 # filtrar aqueles que têm um campo específico faltando
 
-usuarios = [
-    {"nome": "Alice", "email": "alice@example.com"},
-    {"nome": "Bob", "email": ""},
-    {"nome": "Carol", "email": "carol@example.com"}
-]
+# usuarios = [
+#     {"nome": "Alice", "email": "alice@example.com"},
+#     {"nome": "Bob", "email": ""},
+#     {"nome": "Carol", "email": "carol@example.com"}
+# ]
 
-usuarios_com_email_valido = []
+# usuarios_com_email_valido = []
 
-for usuario_atual in usuarios:
-    if usuario_atual["email"]:
-        usuarios_com_email_valido.append(usuario_atual)
+# for usuario_atual in usuarios:
+#     if usuario_atual["email"]:
+#         usuarios_com_email_valido.append(usuario_atual)
 
 
-print(usuarios_com_email_valido)
+# print(usuarios_com_email_valido)
 
 
 
 ### Exercício 9. Extração de Subconjuntos de Dados
 # Objetivo:** Dada uma lista de números, extrair apenas aqueles que são pares.
+
+# numeros_em_geral = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 14, 16, 30, 25, 34]
+# numeros_pares = [numeral for numeral in numeros_em_geral if numeral % 2 ==0]
+# print(numeros_pares)
+
+
+#OUUU
+
+
+numeros_em_geral = range(1,25)
+numeros_pares = [n for n in numeros_em_geral if n % 2 ==0 ]
+print(numeros_pares)
+
 
 ### Exercício 10. Agregação de Dados por Categoria
 # Objetivo:** Dado um conjunto de registros de vendas, calcular o total de vendas por categoria.
