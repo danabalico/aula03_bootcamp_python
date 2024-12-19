6e4752a4cd28e29b7953a343fcbe3510ee64e3a3
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-19 18:46:49 -0300

    Adicionado exercicio if comleto

--- exercicio_if.py
+++ exercicio_if.py
@@ -84,13 +84,14 @@
 # como transacao = {'valor': 12000, 'hora': 20}, verifique se 
 # ela é suspeita.
 
-transacao = {'valor': 12000, 'hora': 20}
+# transacao = {'valor': 12000, 'hora': 20}
 
-if transacao['valor'] > 10000 or transacao ['hora'] < 9 \
-    or transacao['hora'] > 18:
-    print("Transação suspeita")
-else:
-    print("Transação normal")
+# if transacao['valor'] > 10000 or transacao ['hora'] < 9 \
+#     or transacao['hora'] > 18:
+#     print("Transação suspeita")
+# else:
+#     print("Transação normal")
     
+
 
 
