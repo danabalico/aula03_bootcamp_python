2332e5d5ee3c20a4451b2444b3395c74fbf8dfe1
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-20 06:35:48 -0300

    Adicionado exercicio 2

--- exercicios_extras_while.py
+++ exercicios_extras_while.py
@@ -4,14 +4,31 @@
 # conjunto de registros. Crie um programa que gere IDs começando de 1001 até 1010, 
 # e armazene-os em uma lista.
 
-ids = []
-id_atual = 1001
+# ids = []
+# id_atual = 1001
 
-while id_atual <= 1010:
-    ids.append(id_atual)
-    id_atual +=1
-print("IDs gerados: " , ids )
+# while id_atual <= 1010:
+#     ids.append(id_atual)
+#     id_atual +=1
+# print("IDs gerados: " , ids )
 
+
+# 2. Validar formatação de datas
+# Enunciado:
+# Implemente um programa que peça ao usuário para inserir uma 
+# data no formato DD/MM/YYYY. O programa deve continuar pedindo 
+# até que o formato correto seja digitado.
+
+# aqui colocou a condição no while antes de colocar o input
+
+# import re #biblioteca de extressoes regulares
+
+# data_usuario = ""
+
+# while not re.match (r"^\d{2}/\d{2}/\d{4}$", data_usuario): 
+#     data_usuario = input ("Digite uma data no formato DD/MM/AAAA: ")
+
+# print("Data válida inserida: ", data_usuario)
 
 
 
