94c14d7053c3a392bb56f483fbf1c84b7fbfbbf0
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-20 05:03:59 -0300

    Adicionado exercicio 13 de while

--- exercicios_while.py
+++ exercicios_while.py
@@ -14,13 +14,13 @@
 # Solicitar ao usuário um número dentro de 
 #um intervalo específico até que a entrada seja válida.
 
-numero = int(input("Digite um número entre 0 e 10: "))
+# numero = int(input("Digite um número entre 0 e 10: "))
 
-while numero < 0 or numero > 10:
-    print("Número fora do intervalo!")
-    numero = int (input("Por favor, digite um número entre 0 e 10: "))
+# while numero < 0 or numero > 10:
+#     print("Número fora do intervalo!")
+#     numero = int (input("Por favor, digite um número entre 0 e 10: "))
 
-print("Dados válidos") # se nao entrar no while vai mostrar isso
+# print("Dados válidos") # se nao entrar no while vai mostrar isso
 
     
 
@@ -28,6 +28,18 @@ print("Dados válidos") # se nao entrar no while vai mostrar isso
 # Simular o consumo de uma API paginada, 
 #onde cada "página" de dados é processada em loop até 
 #que não haja mais páginas.
+
+pagina_atual = 1
+paginas_totais = 3
+
+while pagina_atual <= paginas_totais:
+    print(f"Processando a página {pagina_atual} de { paginas_totais}")
+    pagina_atual +=1
+
+print ("Todas as páginas foram processadas")
+
+
+
 
 ### Exercício 14. Tentativas de Conexão
 # Simular tentativas de reconexão a um serviço com um 
