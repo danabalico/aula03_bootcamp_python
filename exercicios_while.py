b0241f51d1bdb57956f63c668f4be833a23f61da
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2024-12-20 04:46:21 -0300

    exercicio 11 de while adicionado

---
+++ exercicios_while.py
@@ -0,0 +1,28 @@
+### Exercícios com WHILE
+
+### Exercício 11. Leitura de Dados até Flag
+# Ler dados de entrada até que uma palavra-chave 
+#específica ("sair") seja fornecida.
+
+dados = [] # fazer uma lista vazia
+input_usuario = "" # iniciamos a variavel para nao dar erro no loop
+while input_usuario.lower() != 'sair':
+    input_usuario = input("Digite um valor ( ou 'sair' para terminar): ")
+    
+
+### Exercício 12. Validação de Entrada
+# Solicitar ao usuário um número dentro de 
+#um intervalo específico até que a entrada seja válida.
+
+### Exercício 13. Consumo de API Simulado
+# Simular o consumo de uma API paginada, 
+#onde cada "página" de dados é processada em loop até 
+#que não haja mais páginas.
+
+### Exercício 14. Tentativas de Conexão
+# Simular tentativas de reconexão a um serviço com um 
+#limite máximo de tentativas.
+
+### Exercício 15. Processamento de Dados com Condição de Parada
+# Processar itens de uma lista até encontrar um valor 
+#específico que indica a parada.
