5fb88c076320b485d3a4e8be517443d76daf1826
Jordana Balico <jordanaoliveirasilva1809@gmail.com>
2025-01-06 11:59:11 -0300

    adicionado main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+# x = 2
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+
+
+# # Measure some strings:
+# words = ['cat', 'window', 'defenestrate']
+# for w in words:
+#     print(w, len(w))
+
+# # Measure some strings:
+# nome = ['Luciano']
+# for letra in nome:
+    # print(letra)
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i in range(len(a)):
+#     print(i, a[i])
+
+# OUUU
+
+# a = ['Mary', 'had', 'a', 'little', 'lamb']
+# for i, valor in enumerate(a):
+#     print(i, valor)
